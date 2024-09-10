fbbbb902551cbec8b4ffe6e2076451e035ae7400
shashank das vaishnav <141327449+shashankdas123@users.noreply.github.com>
2024-09-10 13:14:19 +0530

    Update Holdings.jsx

--- src/components/Holdings.jsx
+++ src/components/Holdings.jsx
@@ -15,7 +15,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://backend-dcj7.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
